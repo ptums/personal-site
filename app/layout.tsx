2219a0104be2f67161c9874084461ba22c17d363
ptums <ptumulty923@gmail.com>
2023-03-20 11:18:20 -0400

    removed duplicate char set utf8

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,6 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${lato.className} m-0 p-0`}>
       <head>
-        <meta charSet="utf-8" />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
