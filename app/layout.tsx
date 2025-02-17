4fc4df7ccf3b12802e5e9f01ed76632e9d46986e
ptums <ptumulty923@gmail.com>
2025-02-17 14:06:03 -0500

    added page speed insights tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import Footer from "components/Footer";
 import Header from "components/Header";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   title:
@@ -110,6 +111,7 @@ export default function RootLayout({
           <Footer />
         </div>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
