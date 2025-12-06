8e960002cd226924e57a2e7d751b004b208fed43
ptums <ptumulty923@gmail.com>
2025-12-06 12:04:24 -0600

    added some formating

--- components/Introduction.tsx
+++ components/Introduction.tsx
@@ -15,7 +15,7 @@ const Introduction = () => {
         Software Engineer
       </p>
       <p className="text-base mb-6 mx-0 text-emerald-800 font-normal">
-Building reliable software means more than just writing code—it’s about creating clean, maintainable systems that run smoothly and securely. Like a detective piecing together clues, I focus on understanding the hidden details that keep complex systems safe and dependable. I leverage automation and AI tools to speed up development and reduce errors, making processes smarter and more efficient. Behind the scenes, I manage networks and system infrastructure to ensure everything stays strong and ready to grow. Staying curious, I continuously learn new approaches to software design and monitoring to keep up with today’s fast-changing tech landscape. Ultimately, I bring a big-picture perspective that leads to smarter, more thoughtful solutions beyond just coding.
+Building reliable software means more than just writing code—it’s about creating clean, maintainable systems that run smoothly and securely. Like a detective piecing together clues, I focus on understanding the hidden details that keep complex systems safe and dependable. I leverage automation and AI tools to speed up development and reduce errors, making processes smarter and more efficient. <br/>Behind the scenes, I manage networks and system infrastructure to ensure everything stays strong and ready to grow. Staying curious, I continuously learn new approaches to software design and monitoring to keep up with today’s fast-changing tech landscape. Ultimately, I bring a big-picture perspective that leads to smarter, more thoughtful solutions beyond just coding.
       </p>
     </>
   );
